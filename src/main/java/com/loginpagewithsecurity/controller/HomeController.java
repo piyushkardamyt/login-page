24c98cd120813ea9d52f3e6960b17dbd3c34a42f
PiyushCPP <piyushkardamyt@gmail.com>
2025-10-31 18:34:07 +0530

    add login page

--- src/main/java/com/loginpagewithsecurity/controller/HomeController.java
+++ src/main/java/com/loginpagewithsecurity/controller/HomeController.java
@@ -2,8 +2,10 @@ package com.loginpagewithsecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/login-page")
 public class HomeController {
 	
 	@GetMapping("/")
